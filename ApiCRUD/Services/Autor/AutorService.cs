b48d968ae3d7ccf789c7248e5a7ea8c6949321b2
João Victor <elietejvc@hotmail.com>
2024-10-05 22:12:21 -0300

    Implementando módulos na interface

--- ApiCRUD/Services/Autor/AutorService.cs
+++ ApiCRUD/Services/Autor/AutorService.cs
@@ -1,6 +1,22 @@
-﻿namespace ApiCRUD.Services.Autor
+﻿using ApiCRUD.Models;
+
+namespace ApiCRUD.Services.Autor
 {
-    public class AutorService: IAutorInterface
+    public class AutorService : IAutorInterface
     {
+        public Task<ResponseModel<AutorModel>> BuscarAutorPorId(int idAutor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<AutorModel>> BuscarAutorPorLivro(int idLivro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<List<AutorModel>>> ListarAutores()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- ApiCRUD/Services/Autor/IAutorInterface.cs
+++ ApiCRUD/Services/Autor/IAutorInterface.cs
@@ -1,6 +1,11 @@
-﻿namespace ApiCRUD.Services.Autor
+﻿using ApiCRUD.Models;
+
+namespace ApiCRUD.Services.Autor
 {
     public interface IAutorInterface
     {
+        Task<ResponseModel<List<AutorModel>>> ListarAutores();
+        Task<ResponseModel<AutorModel>> BuscarAutorPorId(int idAutor);
+        Task<ResponseModel<AutorModel>> BuscarAutorPorLivro(int idLivro);
     }
 }
